71332c7461eaf5b14a8263e4116b4d2600894aa7
Renan <machadosantosrenan@gmail.com>
2026-03-29 20:27:26 -0300

    Fixing project Structure

--- target/debug/deps/prey-6ebb031dd3575e17.d
+++
@@ -1,5 +0,0 @@
-/home/renan/Documents/prey/target/debug/deps/prey-6ebb031dd3575e17.d: src/lib.rs
-
-/home/renan/Documents/prey/target/debug/deps/libprey-6ebb031dd3575e17.rmeta: src/lib.rs
-
-src/lib.rs:
